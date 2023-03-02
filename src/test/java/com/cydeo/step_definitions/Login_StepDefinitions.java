ad06dcd45e0a5b7710b806add2e8954c90b7cab4
caliskanemine <caliskanemine95@gmail.com>
2023-03-02 17:09:51 +0200

    db tag is deleted

---
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -0,0 +1,62 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_StepDefinitions {
+
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("User enters librarian username");
+
+        // throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("User enters librarian password");
+
+        // throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("User should see the dashboard");
+
+        // throw new io.cucumber.java.PendingException();
+
+    }
+
+
+    @When("user enters student username")
+    public void userEntersStudentUsername() {
+        System.out.println("User enters student username");
+    }
+
+    @And("user enters student password")
+    public void userEntersStudentPassword() {
+        System.out.println("User enters student password");
+    }
+
+
+    @When("user enters admin username")
+    public void userEntersAdminUsername() {
+        System.out.println("User enters admin username");
+    }
+
+    @And("user enters admin password")
+    public void userEntersAdminPassword() {
+        System.out.println("User enters admin password");
+    }
+
+
+    @Given("user is on the library login page")
+    public void userIsOnTheLibraryLoginPage() {
+        System.out.println("User is on the library login page");
+    }
+}
